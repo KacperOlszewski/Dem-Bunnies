0b3552f03f6f3ca0ca5b87e983a81766f6d130f5
kacperolszewski <kacperolszewski@o2.pl>
2015-05-21 14:42:37 +0200

    black rabbit chance

--- js/bunny.js
+++ js/bunny.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
 
-/*	for (var i = 1; i < n; i++) { */
-
   $('.rubber').click(function() {
     randoms();
 
@@ -9,20 +7,21 @@ $(document).ready(function() {
   })
 });
 
-var [i, points, score, combo, stack] = [0, 0, 0, -1, 0]; 
+var [i, points, score, combo, stack] = [0, 0, 0, -1, 1]; 
 
 function randoms(n) { 
-    var limit = 100;
-    var initial = 4000;
-    var max_speed = 900 / initial;
-    var speed = initial - (i/limit * initial * (1-max_speed));
+    var [limit, initial, bunny_duration] = [100, 3000, 800];
+    var progress = i/limit;
+    var max_speed = bunny_duration / initial;
+    var speed = initial - (progress * initial * (1-max_speed));
     var random = [1,2,3,4,5,6,7,8,9][Math.floor(Math.random()*9)];
+    var black = Math.random();
 
     $('#speed').html(speed+" "+max_speed);
 
     setTimeout(function () 
     {
-      spawn(random);
+      spawn(random, black, progress);
         i++; 
         if (i < (limit-1) )
         { 
@@ -31,42 +30,54 @@ function randoms(n) {
         else
         {
           $('.rubber').show("fast");
-          return [i, points, score, combo] = [0, 0, 0, -1]; 
+          return [i, points, score, combo, stack] = [0, 0, 0, -1, 1]; 
         }
     }, speed)
 
     setTimeout(function () {
           destroy(random);
-    }, speed+899);
+    }, speed+(bunny_duration-1));
 
-    $('span.jump').click(function() {
+    $('span.white').click(function() {
 
         $(this).hide();
         points++;
         if (i == combo+1) {
             stack++;
             if (stack % 5 != 0) {
-            hit = 10 * (Math.round(i/limit*10) / 5 + 1); 
+              hit = 10 * (Math.round(progress*10) / 5 + 1); 
             }
             else {
-            wombo = 2*(stack/5);
-            hit = wombo * 10 * (Math.round(i/limit*10) / 5 + 1); 
-            $('#wombo').html(wombo+"X !!!"+stack);  
+              wombo = 2*(stack/5);
+              hit = wombo * 10 * (Math.round(progress*10) / 5 + 1); 
+              $('#wombo').html(wombo+"X !!!"+stack);  
             }
         }       
         else {
-            hit = 10 * (Math.round(i/limit*10) / 5 + 1); 
-            stack = 0;
+            hit = 10 * (Math.round(progress*10) / 5 + 1); 
+            stack = 1;
         };
         score += hit;     
         combo = i;
         $('#points').html(Math.round(score));
     });
+
+    $('span.black').click(function() {
+        $(this).hide();
+        score *= 0.8;     
+        stack = 1;
+        $('#points').html(Math.round(score));
+    });
    // return i = 0; infinite
 }
 
-function spawn(random) {
-    $(".rabbit"+random).append("<span class='jump'>"+random+"</span>");
+function spawn(random, black, progress) {
+  if (black < (progress*0.5)) {
+    $(".rabbit"+random).append("<span class='jump black'>"+random+"</span>");
+  }
+  else {
+    $(".rabbit"+random).append("<span class='jump white'>"+random+"</span>");
+  }
 }
 function destroy(random) {
     $(".rabbit"+random+" span").remove();
